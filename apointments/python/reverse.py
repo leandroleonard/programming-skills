264376f65d2aad8dd3e91437dd93628c0e64d84c
leandroleonard <leandroventur19@gmail.com>
2025-06-23 04:19:39 -0300

    refactor: test case

--- apointments/python/reverse.py
+++ apointments/python/reverse.py
@@ -20,8 +20,10 @@ def max_upper_left_submatrix(matrix):
 #     [0, 4, 1, 8]
 # ]
 matrix = [
-    [1, 2],
-    [3, 4],
+    [112, 42, 83, 119],
+    [56, 125, 56, 49],
+    [15, 78, 101, 43],
+    [62, 98, 114, 108]
 ]
 
 print(max_upper_left_submatrix(matrix)) 
