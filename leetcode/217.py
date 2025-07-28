40fc47d6ccf401148a1ea1c90feb262461391ff0
leandroleonard <leandroventur19@gmail.com>
2025-07-28 04:33:48 -0300

    Leetcode Problem 217. Contains Duplicate - Python

---
+++ leetcode/217.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = {}
+        
+        for i in nums:
+            if i not in count:
+                count[i] = 1
+            else:
+                return True
+        return False
